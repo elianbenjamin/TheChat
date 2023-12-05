ac611f2a6f7fbb104dd29b7c80876e323d7983f7
Mariano <marianoaguirre14@gmail.com>
2023-12-05 10:27:49 -0300

    Skeleton front 2.0

---
+++ client/src/components/ChatContainer/ChatContainer.tsx
@@ -0,0 +1,16 @@
+"use client";
+import React from 'react';
+
+export type ChatContainerProps = {
+	// types...
+}
+
+const ChatContainer: React.FC<ChatContainerProps>  = ({}) => {
+	return (
+		<div>
+ 			ChatContainer works!
+ 		</div>
+	);
+};
+
+export default ChatContainer;
